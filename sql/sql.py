4bf669a6488f92c2624a96084f6b9c0900ed8d69
my <alejodil98@gmail.com>
2022-06-20 12:42:51 -0300

    version 2

--- sql/sql.py
+++ sql/sql.py
@@ -4,10 +4,13 @@ conn = sqlite3.connect('pruebaSQL.db')
 
 c = conn.cursor()
 
-# CREATE = creacion de la tabla clients
+# CREATE = Crea la tabla clients
 c.execute("CREATE TABLE IF NOT EXISTS clients (first_name, last_name, birth, email)")
 
-# INSERT = agregar valores a la tabla clientes
+
+# INSERT = Agrega valores a la tabla clients
+print("\nMetodo INSERT - Valores de la tabla clients")
+
 with conn:
     c.execute("INSERT INTO clients values (?,?,?,?)", ('Kevin', 'Harnan', 22, 'kharnan@ucema.edu.ar'))
     c.execute("INSERT INTO clients values (?,?,?,?)", ('Alejo', 'Di Lelle', 24, 'adilelle@ucema.edu.ar'))
@@ -17,16 +20,28 @@ with conn:
 c.execute("SELECT * FROM clients")
 print(c.fetchall())
 
-# UPDATE = agregar valores a la tabla clientes
+
+# UPDATE = Actualiza valores de la tabla clients, en este caso cambia la edad de Alejo de 24 a 23 años
+print("\nMetodo UPDATE - Actualiza la tabla clients")
+
 with conn:
     c.execute("UPDATE clients set birth = '23' WHERE first_name = 'Alejo' AND last_name = 'Di Lelle'")
+
+c.execute("SELECT * FROM clients WHERE last_name = 'Di Lelle'")
+print(c.fetchall())
+
+
+# DELETE = Borra el registro del cliente "Catalina Dapena" la tabla clients
+print("\nMetodo DELETE - Elimina de la tabla clients")
+
+with conn:
+    c.execute("DELETE FROM clients WHERE first_name = 'Catalina' AND last_name = 'Dapena'")
 
 c.execute("SELECT * FROM clients")
 print(c.fetchall())
 
-# DELETE = borrar el registro del cliente "Catalina Dapena" la tabla clientes
-with conn:
-    c.execute("DELETE FROM clients WHERE first_name = 'Catalina' AND lastname = 'Dapena'")
+# SELECT = Selecciona en la tabla clients
+print("\nMetodo SELECT - Selecciona un cliente de la tabla clients")
 
-c.execute("SELECT * FROM clients")
+c.execute("SELECT * FROM clients WHERE first_name = 'Kevin' AND last_name = 'Harnan'")
 print(c.fetchall())
